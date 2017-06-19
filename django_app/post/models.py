5a991fb68ebac32aee1dcd5c6fff4ec5084e6b3a
GAIUS8 <gaius827@gmail.com>
2017-06-19 20:44:41 +0900

    Create model Post

--- django_app/post/models.py
+++ django_app/post/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    photo = models.ImageField(upload_to='post', blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-pk', ]
+
